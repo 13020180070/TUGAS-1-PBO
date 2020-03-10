8e3aebae089cc3880d90464a9cb2802c4b1bfd3d
Siti Halima <62000373+13020180070@users.noreply.github.com>
2020-03-10 19:08:26 +0800

    Add files via upload

---
+++ TUGAS PBO/halima.java
@@ -0,0 +1,5 @@
+public class halima{
+	public static void main(String[] args){
+	System.out.println("Selamat Belajar");
+	}
+}
---
+++ TUGAS PBO/pemogramanJava.java
@@ -0,0 +1,5 @@
+public class pemogramanJava{
+	public static void main(String[] args){
+	System.out.println("Selesai Belajar Pemograman java");
+	}
+}
